840b367ba8a845aed5e86b357c5f32ed98eec8a1
jocelflores <jocelynflores@utexas.edu>
2020-08-22 19:07:30 -0500

    Month Count added

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,16 @@ import csv
 # path to pull data from the Resources folder
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath, 'r') as csvfile:
+month_count = 0
+net_profit = 0
+
+with open(csvpath, ) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    for row in csvreader:
+        month_count = month_count + 1
+        #net_profit = net_profit + int(row["Profit/Losses"])
 
-    print("hello")
+# the minus one is to account for the header
+print(f"Total Months: {month_count-1}")
